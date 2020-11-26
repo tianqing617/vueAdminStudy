030db75ff1ed51435cb82cc894ee5cfb8979691f
刘天青 <liutianqing@dfsoft.com.cn>
2020-11-26 15:18:29 +0800

    feat: test

--- src/views/guide/index.vue
+++ src/views/guide/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-container">
     <aside>
-      The guide page is useful for some people who entered the project for the first time. You can briefly introduce the
+      The guide8888888888 page is useful for some people who entered the project for the first time. You can briefly introduce the
       features of the project. Demo is based on
       <a href="https://github.com/kamranahmedse/driver.js" target="_blank">driver.js.</a>
     </aside>
